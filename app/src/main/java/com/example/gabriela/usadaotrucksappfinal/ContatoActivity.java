3ddbc4cb9031b8a31a5edbdad63bc8e6cd7bf86f
daniela sampaio natividade <danielasampaionatividade@gmail.com>
2018-11-12 17:48:27 -0200

    feito varias alterações

---
+++ app/src/main/java/com/example/gabriela/usadaotrucksappfinal/ContatoActivity.java
@@ -0,0 +1,37 @@
+package com.example.gabriela.usadaotrucksappfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ContatoActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contato);
+
+        spinner= (Spinner) findViewById(R.id.spinner);
+
+      ArrayAdapter adapter=ArrayAdapter.createFromResource(this,R.array.selecione,android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this,"Voce selecionou",Toast.LENGTH_SHORT).show();
+        TextView myText=(TextView) view;
+        Toast.makeText(this,"Você selecionou"+myText.getText(),Toast.LENGTH_SHORT).show();
+
+    }
+
+}
---
+++ app/src/main/java/com/example/gabriela/usadaotrucksappfinal/DeletarActivity.java
@@ -0,0 +1,40 @@
+package com.example.gabriela.usadaotrucksappfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class DeletarActivity extends AppCompatActivity {
+
+    //chamado p iniciar uma atividade
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deletar);
+    }
+
+    public void deletar(View view) {
+        PecasCrudRemoto c = new PecasCrudRemoto();
+         //findviewbyid=Esse método é utilziado para buscar e recupera as Views dentro do layout
+        EditText etNome_peca = findViewById(R.id.etNome_peca);
+        EditText etVeiculo = findViewById(R.id.etVeiculo);
+        EditText etAno = findViewById(R.id.etAno);
+
+        //abaixo get.text e to.string ele faz é retornar uma representação textual em String do objeto em questão.
+        //POST: Utilizado para inserir dados remotamente.
+        c.execute("POST", etNome_peca.getText().toString(),
+                etVeiculo.getText().toString(),
+                etAno.getText().toString());
+        finish();
+
+    }
+
+    //Para encerrar uma atividade, chame o método finish()
+    public void cancelar(View v) {
+        finish();
+    }
+
+}
+
+
